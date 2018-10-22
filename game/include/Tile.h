219489f36d1328046f8fddf9776c012109457850
Jesse Blacklock <jesse.blacklock@student.csulb.edu>
2018-10-21 20:07:47 -0700

    adjust Tile.h and Tile.cpp

--- game/include/Tile.h
+++ game/include/Tile.h
@@ -1,11 +1,26 @@
 #ifndef TILE_H
 #define TILE_H
-
+#include <string>
 
 class Tile
 {
     public:
-        Tile();
+        //name of BMP file
+        const char* imageString;
+        //actual name of tile
+        std::string tileName;
+        //tile's position on the x-axis
+        int x;
+        //tile's position on the y-axis
+        int y;
+        int indexWeight;
+        //I have questions about the following three member variables
+        SDL_Rect srcrect;
+        SDL_Rect dstrect;
+        SDL_Texture *texture;
+
+        //what parameters should this take???
+        void renderTile();
 
     protected:
 
