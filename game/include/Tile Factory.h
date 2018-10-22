b42acfb329e283f4aba53610cb96ea571f1042f2
Jesse Blacklock <jesse.blacklock@student.csulb.edu>
2018-10-21 19:38:14 -0700

    Foundations

---
+++ game/include/Tile Factory.h
@@ -0,0 +1,15 @@
+#ifndef TILE FACTORY_H
+#define TILE FACTORY_H
+
+
+class Tile Factory
+{
+    public:
+        Tile Factory();
+
+    protected:
+
+    private:
+};
+
+#endif // TILE FACTORY_H
---
+++ src/Tile Factory.cpp
@@ -0,0 +1,6 @@
+#include "Tile Factory.h"
+
+Tile Factory::Tile Factory()
+{
+    //ctor
+}
