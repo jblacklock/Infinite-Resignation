329465d4cf0482d7bd9074f7d5693162334fe657
James <iamjimmyjimmy@yahoo.com>
2018-11-22 01:18:30 -0800

    Fixed Credit Label Scaling
    Fixed Scaling Issues with text of the credit screen

--- game/src/Menu/CreditState.cpp
+++ game/src/Menu/CreditState.cpp
@@ -1,22 +1,43 @@
 #include "CreditState.h"
+#include <string>
 kiss_button cBackButton = {0};
-kiss_label credit,text = {0};
+kiss_label credit,textL = {0};
 
-char *creditTitle = "CREDITS";
-char *designers = "";
-char *artist= "";
-char *developers= "";
-char *levelDesigner= "";
-char *mostDeadInside= "";
-char *sound= "";
+char *creditTitle = (char*)"CREDITS";
+
+std::string text =
+            "Designer              Artist\n\n"+
+std::string("Jimmy Chao          Christina Chao\n")+
+std::string("\n\n\n\n")+
+std::string("Level Designer        Developers\n\n")+
+std::string("Jesse Blacklock        Jimmy Chao\n")+
+std::string("                      Jesse Blacklock\n") +
+std::string("\n\n\n\n")+
+std::string("Dead Inside           Sound\n\n") +
+std::string("Jimmy Chao          Jimmy Chao");
+
+std::string textXL =
+            "Designer              Artist\n\n\n"+
+std::string("Jimmy Chao          Christina Chao\n\n\n")+
+std::string("\n\n\n\n")+
+std::string("Level Designer        Developers\n\n\n")+
+std::string("Jesse Blacklock        Jimmy Chao\n\n\n")+
+std::string("                      Jesse Blacklock\n\n\n") +
+std::string("\n\n\n\n")+
+std::string("Dead Inside           Sound\n\n\n") +
+std::string("Jimmy Chao          Jimmy Chao");
 
 CreditState::CreditState(MenuScreen* menu)
 {
     this->menu = menu;
-    kiss_button_new(&cBackButton,&WindowProperty::window,"Back",0,0,100,50);
+    kiss_button_new(&cBackButton,&WindowProperty::window,(char*) "Back",0,0,100,50);
     kiss_label_new(&credit,&WindowProperty::window,creditTitle,150,50);
+    kiss_label_new(&textL,&WindowProperty::window,(char*)text.c_str(),50,150);
     cBackButton.font.font = TTF_OpenFont("assets/fonts/standard.ttf", 20);
     credit.font.font = TTF_OpenFont("assets/fonts/standard.ttf",40);
+    textL.font.font = TTF_OpenFont("assets/fonts/standard.ttf",15);
+    credit.textcolor = {255, 255, 255, 255};
+    textL.textcolor = {255, 255, 255, 255};
 }
 
 CreditState::~CreditState()
@@ -27,6 +48,7 @@ CreditState::~CreditState()
 void CreditState::render(){
     kiss_button_draw(&cBackButton,WindowProperty::renderer);
     kiss_label_draw(&credit,WindowProperty::renderer);
+    kiss_label_draw(&textL,WindowProperty::renderer);
 }
 
 void CreditState::update(){
@@ -38,6 +60,21 @@ void CreditState::update(){
     cBackButton.textx  = 10 * WindowProperty::getWidthDisposition();
     cBackButton.texty  = 5 * WindowProperty::getHeightDisposition();
     cBackButton.font.font = TTF_OpenFont("assets/fonts/standard.ttf", 20 *diag);
+    credit.rect.x = 150* WindowProperty::getWidthDisposition();
+    credit.rect.y = 50 * WindowProperty::getHeightDisposition();
+    credit.font.font = TTF_OpenFont("assets/fonts/standard.ttf", 40 *diag);
+
+    textL.rect.x = 50* WindowProperty::getWidthDisposition();
+    textL.rect.y = 150 * WindowProperty::getHeightDisposition();
+
+    textL.font.font = TTF_OpenFont("assets/fonts/standard.ttf", 15 *diag);
+    if((15 *diag)>30){
+        strcpy(textL.text,textXL.c_str());
+    }
+    else{
+        strcpy(textL.text,text.c_str());
+
+    }
 
 }
 
